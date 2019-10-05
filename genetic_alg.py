3738671b797e6385ab393f78f56d2ef841697b04
panos-stavrianos <pstavrianosmail@gmail.com>
2019-10-05 10:11:08 +0300

    Initial commit

---
+++ genetic_alg.py
@@ -0,0 +1,69 @@
+import random
+
+from pyeasyga import pyeasyga
+from pyeasyga.pyeasyga import Chromosome
+
+from main import *
+
+desired = 50000
+mr = [int(desired / 2)]
+
+data = [desired] * length * 5
+ga = pyeasyga.GeneticAlgorithm(data,
+                               population_size=200,
+                               generations=100000,
+                               mutation_probability=1,
+                               crossover_probability=0.00,
+                               elitism=False,
+                               maximise_fitness=False)
+
+
+def mutate(individual):
+    if mr[0] == 0:
+        mr[0] = 1
+    for i in range(16):
+        mutate_index = random.randrange(len(individual))
+        val = individual[mutate_index] + random.randrange(-mr[0], mr[0])
+        if val < 0:
+            val = 0
+        individual[mutate_index] = val
+
+
+# define and set function to create a candidate solution representation
+def create_individual(data):
+    individual = [random.randrange(0, desired) for i in range(length * length)]
+    return individual
+
+
+ga.create_individual = create_individual
+ga.mutate_function = mutate
+ga.fitness_function = fitness  # set the GA's fitness function
+
+
+def run():
+    """Run (solve) the Genetic Algorithm."""
+    ga.create_first_generation()
+    prev = ''
+    removing = 0
+
+    for i in range(1, ga.generations):
+        ga.create_next_generation()
+        mr[0] = int(ga.best_individual()[0] / 5)
+        if ga.best_individual()[0] == 0:
+            return ga.best_individual()
+        if i % 10 == 0:
+            print(i, ga.best_individual())
+        if prev == ga.best_individual():
+            removing += 1
+            # for j in range(10):
+            #     ga.current_generation[j] = Chromosome(create_individual(data))
+        if removing > 5:
+            removing = 0
+            print("removing")
+
+            for j in range(int(20)):
+                ga.current_generation[j] = Chromosome(create_individual(data))
+        prev = ga.best_individual()
+
+
+print(run())
---
+++ main.py
@@ -0,0 +1,123 @@
+import itertools
+
+import numpy as np
+from progressbar import *
+from scipy.spatial import distance
+
+length = 4
+max_num = 10
+
+
+def columns(table):
+    r = []
+    for y in range(0, length):
+        sum = 0
+        for x in range(0, length):
+            sum += table[x][y]
+        r.append(sum)
+    return r
+
+
+def rows(table):
+    r = []
+    for x in range(0, length):
+        sum = 0
+        for y in range(0, length):
+            sum += table[x][y]
+        r.append(sum)
+    return r
+
+
+def diagonals(table):
+    r = []
+    sum = 0
+    for i in range(0, length):
+        sum += table[i][i]
+    r.append(sum)
+    r.append(sum)
+    sum = 0
+    for i in range(0, length).__reversed__():
+        sum += table[i][i]
+    r.append(sum)
+    r.append(sum)
+    return r
+
+
+def opposites(table):
+    r = []
+    for i in range(0, 4):
+        sum = 0
+        sum += table[i][i]
+        sum += table[i][1 - i]
+        sum += table[1 - i][i]
+        sum += table[1 - i][1 - i]
+        r.append(sum)
+    return r
+
+
+def pair_groups(table):
+    r = []
+    for x in range(0, length - 1):
+        for y in range(0, length - 1):
+            print(x, y)
+            sum = 0
+            sum += table[x][y]
+            sum += table[x][y + 1]
+            sum += table[x + 1][y]
+            sum += table[x + 1][y + 1]
+            r.append(sum)
+    return r
+
+
+def groups(table):
+    r = []
+    for x in range(0, length - 1, 2):
+        for y in range(0, length - 1, 2):
+            sum = 0
+            sum += table[x][y]
+            sum += table[x][y + 1]
+            sum += table[x + 1][y]
+            sum += table[x + 1][y + 1]
+            r.append(sum)
+    return r
+
+
+def fitness(table, target_val):
+    target_val = list((map(list, zip(*[iter(target_val)] * length))))
+    table = list((map(list, zip(*[iter(table)] * length))))
+
+    target = np.array([target_val[0]])
+    results = np.array([columns(table), rows(table), diagonals(table), opposites(table), groups(table)])
+    dist = sum(sum(distance.cdist(results, target, 'euclidean')))
+    return int(dist)
+
+
+def test():
+    table = [
+        [10, 3, 13, 19],
+        [17, 15, 13, 0],
+        [10, 5, 12, 18],
+        [8, 22, 7, 8]
+    ]
+    print(fitness(table, 46))
+
+
+def brute_force():
+    count = 0
+    loops_length = (max_num ** (length * length))
+
+    widgets = ['Test: ', Percentage(), ' ', Bar(marker='#', left='[', right=']'),
+               ' ', ETA(), ' ', SimpleProgress()]  # see docs for other options
+
+    pbar = ProgressBar(widgets=widgets, maxval=loops_length)
+    pbar.start()
+
+    for x in itertools.product(range(max_num), repeat=length * length):
+        table = list((map(list, zip(*[iter(x)] * length))))
+        # validate(table)
+        count += 1
+
+        if count % 10000 == 0:
+            pbar.update(count)  # this adds a little symbol at each iteration
+
+    pbar.finish()
